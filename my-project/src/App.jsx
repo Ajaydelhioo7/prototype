98b3d5fdd0512d6b5eda2d825b210516dd68a622
Ajaydelhi007 <ajayupadhyay030@gmail.com>
2024-04-03 23:13:13 +0530

    first commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Dropdown from "./components/Dropdown";
+
+const App = () => {
+  return (
+    <div className="mt-5 ml-5">
+      <Dropdown></Dropdown>
+    </div>
+  );
+};
+
+export default App;
